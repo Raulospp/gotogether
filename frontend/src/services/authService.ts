59c553a77ac080ef45e3eea47f296131102ebd0e
Jhon99m <monvalencia1028@gmail.com>
2026-04-13 09:51:12 -0500

    modificaciones: actualización URL render

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://gotogether-o7cl.onrender.com';
+const API_URL = 'https://gotogether-nhuj.onrender.com';
 
 const api = axios.create({ baseURL: API_URL });
 
